ffda6a944d505e25e084c8d81f73939d2859f215
Alperen Teke <alperentkee11@gmail.com>
2024-02-23 00:57:35 +0300

    Add project files.

---
+++ pingServer/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace pingServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 332);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hostname:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kontrol Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(318, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1034, 365);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 19;
+            this.listBox2.Location = new System.Drawing.Point(318, 385);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1034, 365);
+            this.listBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1358, 752);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ pingServer/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pingServer
+{
+    public partial class Form1 : Form
+    {
+
+        private Timer timer;
+        private string kullaniciAdi;
+        private DateTime gonderiZamani;
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string inputHost = textBox1.Text;
+            testConn(inputHost);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            gonderiZamani = DateTime.Now;
+        }
+
+        private string userAccName()
+        {
+            string userName = Environment.UserName;
+            return userName;
+        }
+
+        private void testConn(string txtHost)
+        {
+            string hedefHost = txtHost;
+            if (hedefHost != " " && hedefHost != "" && hedefHost != null)
+            {
+                try
+                {
+                    Ping pingSender = new Ping();
+                    PingReply pingReply = pingSender.Send(hedefHost);
+                    DateTime basariZamani = DateTime.Now;
+                    string basariDurum;
+
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        label2.Text = " ";
+                        basariDurum = "Bağlantı Başarılı!";
+                        listBox1.Items.Add($"Kullanıcı Adı: {kullaniciAdi} Giden Host: {hedefHost} Başarı Durumu: {basariDurum} Gönderi Zamanı: {gonderiZamani} Başarı Zamanı: {basariZamani}");
+                    }
+                    else if (pingReply.Status == IPStatus.TimedOut)
+                    {
+                        label2.Text = "Bağlantı Zaman Aşımı!";
+                        basariDurum = "Bağlantı Zaman Aşımı!";
+                        listBox2.Items.Add($"Kullanıcı Adı: {kullaniciAdi} Giden Host: {hedefHost} Başarı Durumu: {basariDurum} Gönderi Zamanı: {gonderiZamani} Başarı Zamanı: {basariZamani}");
+                    }
+                    else if (pingReply.Status != IPStatus.Success)
+                    {
+                        label2.Text = "Bağlantı Başarısız!";
+                        basariDurum = "Bağlantı Başarısız!";
+                        listBox2.Items.Add($"Kullanıcı Adı: {kullaniciAdi} Giden Host: {hedefHost} Başarı Durumu: {basariDurum} Gönderi Zamanı: {gonderiZamani} Başarı Zamanı: {basariZamani}");
+                    }
+                    else
+                    {
+                        label2.Text = "Tanımlanmamış Hata!";
+                    }
+                }
+                catch
+                {
+                    label2.Text = "İnternet Bağlantısı Bulunamadı!";
+                }
+            }
+            else
+            {
+                label2.Text = "Bir Host Adı Giriniz!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kullaniciAdi = userAccName();
+        }
+    }
+}
